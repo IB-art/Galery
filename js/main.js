022916a6f117fa935cf7072bd9b85c7880a2ef7a
IB-art <65600199+IB-art@users.noreply.github.com>
2024-10-28 13:00:07 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -58,6 +58,18 @@ $(function(){
 		},
 	});
 
+  $('.gallery__inner-5').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+	});
+
 });
 
 
